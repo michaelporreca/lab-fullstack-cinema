40cf82d31569cf5ac1d1764d72d3d6d0eb0b9645
michaelporreca <michaelporreca@gmail.com>
2021-09-25 12:06:44 -0700

    completed iteration 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+    <Switch>
+    <Route path="/movies">
+        <Movies />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
     <div className="App">
-        <h1>Cinema Ironack</h1>
-        <button>Check the movies</button>
+      <Link to="/"><h1>Cinema Ironack</h1></Link>
+      <Link to="/movies" className="button">Check the movies</Link>
+    </div>
+  );
+}
+
+function Movies() {
+  return (
+    <div className="App">
+      <Link to="/"><h1>Cinema Ironack</h1></Link>
+      <Link to="/movies" className="button">Check the movies</Link>
+      <h2>Movies</h2>
     </div>
   );
 }
